27717ac6a66cefab0ca0a9507aa97f0fad92ed6d
aarrasseayoub01 <aarrasseayoub@gmail.com>
2023-10-18 19:05:52 +0100

    initial changes

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Java', 'Node.js', 'Go'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,24 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Ayoub and I live and breath backend work, be it its conception, realization and monitoring, you name it! 
+              My hope is to become, one day, a fully fledged software architect. Until then I will be sharpening my skills and broadening my circle of knowledge eagerly!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://oracle.com/">Oracle</a>,{' '}
+              <a href="https://smilemate.vercel.app/">SmileMate</a>, and{' '}
+              <a href="https://www.apple.com/">Nooba health</a>. My
+              main focus these days is building scalable, highly available distributed systems.
             </p>
 
             <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+                started blogging
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              about my golang based ongoing distributed file system project.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,17 +60,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Ayoub AARRASSE.</h2>;
+  const three = <h3 className="big-heading">I build Backends!</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
+        I’m a software engineer specializing in building and designing reliable
+        digital systems. Currently, I’m focused on building distributed systems, specifically a distributed file system which I'm bloggin about at Medium
         .
       </p>
     </>
